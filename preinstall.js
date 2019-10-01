400dc012befb41756667d090f89d016dc0447509
circuit-monkey <p.ivcec@gmail.com>
2019-10-01 14:10:53 -0400

    debug config vars

--- preinstall.js
+++ preinstall.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 console.log("process.env.GOOGLE_CONFIG", process.env.GOOGLE_CONFIG);
 
 fs.writeFile(
-  "./google-credentials-heroku.json",
+  `${__dirname}/google-credentials-heroku.json`,
   process.env.GOOGLE_CONFIG,
   error => {
     console.log("error writing credentials file", error);
